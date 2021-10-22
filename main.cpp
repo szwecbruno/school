bf5522d094d6ad3b713532a96cb89c8b29d86ede
szwecbruno <90031822+szwecbruno@users.noreply.github.com>
2021-10-22 11:23:57 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,21 +2,80 @@
 
 using namespace std;
 
-class Time {
+class Car {
 	public:
-	static string s_time;
-	static string s_date;
-	static void getData();	
-};
-string Time::s_time="11:03";
-string Time::s_date="01.10.2021";
+	uint id;
+	string brand;
+	string model;
+	void getData();#include <iostream>
 
-void Time::getData(){
-	cout<<"data i godzina:" <<s_time<< " " <<s_date;
+using namespace std;
+
+class Car {
+	
+	int id;
+	string brand;
+	string model;
+	void getData();
+	Car ();
+	
+	Car(int pId, string pBrand, string pModel);
+};
+
+void Car::getData(){
+	cout<<"id: "<<id<<endl;
+	cout<<"brand: "<<brand<<endl;
+	cout<<"model: "<<model<<endl;
+}
+
+Car::Car(){
+	id=0;
+	brand="Marka domyslna";
+	model="model domyslny";
+	cout<<"konstruktor domyslny"<<endl;
+}
+	Car(unsigned int pId, string pmodel, string pBrand){
+	id=pId;
+	model=pModel;
+	brand=pBrand;
+	cout<<"konstruktor parametryczny"<<endl;
 }
 
 int main(int argc, char** argv) {
-	Time::getData();
+	Car k1{1, "ferrari", "f460"};
+	kl.getData();
+	Car k2= Car(10, "bmw");
+	k2.getData();
+	return 0;
+}
+	Car ();
 	
+	Car(int pId, string pBrand, string pModel);
+};
+
+void Car::getData(){
+	cout<<"id: "<<id<<endl;
+	cout<<"brand: "<<brand<<endl;
+	cout<<"model: "<<model<<endl;
+}
+
+Car::Car(){
+	id=0;
+	brand="Marka domyslna";
+	model="model domyslny";
+	cout<<"konstruktor domyslny"<<endl;
+}
+Car::Car(int pId, string pModel, string pBrand){
+	id=pId;
+	model=pModel;
+	brand=pBrand;
+	cout<<"konstruktor parametryczny"<<endl;
+}
+
+int main(int argc, char** argv) {
+	Car k1= car{, "ferrari", "f460"};
+	k1.getData();
+	Car k2= Car(10, "bmw", "x6");
+	k2.getData();
 	return 0;
 }
