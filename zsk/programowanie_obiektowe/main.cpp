a647f66aece5e3e76825efb0b6b9177953f4630f
szwecbruno <90031822+szwecbruno@users.noreply.github.com>
2021-11-25 22:38:22 +0100

    Add files via upload

--- zsk/programowanie_obiektowe/main.cpp
+++ zsk/programowanie_obiektowe/main.cpp
@@ -1,22 +1,71 @@
 #include <iostream>
 
 using namespace std;
-
-class Time {
-	public:
-	static string s_time;
-	static string s_date;
-	static void getData();	
+// stworzenie klasy
+class Car{
+public:
+   //inicjalizacja zmiennych
+    string brand;
+    string model;
+    string color;
+    int price;
+    int year;
+    //inicjalizacja metody
+    void showData();
+    //konstruktor domyslny
+    Car(){
+        cout<<"konstruktor domyœlny"<<endl;
+    };
+    //kontruktory parametryczne
+    Car(string, string, int);
+    Car(string, string, string, int, int);
+    //destruktor
+    ~Car(){
+        cout<<"destruktor"<<endl;
+    }
 };
-string Time::s_time="11:03";
-string Time::s_date="01.10.2021";
+// inicjalizacja konstruktorow poza klasa
+Car::Car(string pBrand, string pModel, int pYear){
+    brand=pBrand;
+    model=pModel;
+    year=pYear;
+}
+Car::Car(string pBrand, string pModel, string pColor, int pYear, int pPrice){
+    brand=pBrand;
+    model=pModel;
+    year=pYear;
+    color=pColor;
+    price=pPrice;
 
-void Time::getData(){
-	cout<<"data i godzina:" <<s_time<< " " <<s_date;
+}
+//inicjalizacja metody
+void Car::showData(){
+cout<<"brand = "<<brand<<endl;
+cout<<"model = "<<model<<endl;
+cout<<"year = "<<year<<endl;
+cout<<"color = "<<color<<endl;
+cout<<"price = "<<price<<endl;
+}
+//funkcja creatobject
+void createObjectCar(){
+	Car car2 = {"bmw", "r8", "szary", 1324, 60000};
+	car2.showData();
 }
 
-int main(int argc, char** argv) {
-	Time::getData();
-	
-	return 0;
+
+int main()
+{
+    //przypisanie wartosci obiektowi
+    Car car1= {"fiat", "multipla", "szary", 1998, 20000};
+    //wywolanie funkcji
+    car1.showData();
+    //uzycie wsakznika
+    Car *car;
+    car =&car1;
+    car->price=2;
+    car->showData();
+    // wywolanie funkcji create object
+    createObjectWorker();
+
+    return 0;
 }
